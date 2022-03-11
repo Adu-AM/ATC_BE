fd768853abf7babd41f59ed199beea5e9f7bbdc5
Bruma Sebastian <SebastianBruma24@gmail.com>
2022-03-11 11:46:06 +0200

    Added update informations for user, activate/deactivate account

--- Controllers/UserManagementController.cs
+++ Controllers/UserManagementController.cs
@@ -81,16 +81,126 @@ namespace ATC_BE.Controllers
         }
 
 
+        [HttpGet]
+        [Route("get-user/{email}")]
+        public async Task<IActionResult> GetUser(string email)
+        {
+            var user = await _dbContext.UserDetails.FindAsync(email);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
 
         [HttpGet]
         [Route("get-users")]
         public async Task<IActionResult> GetUsers()
         {
-
             var users = await _dbContext.UserDetails.ToListAsync();
-            var jsonString = JsonSerializer.Serialize(users);
+            //var jsonString = JsonSerializer.Serialize(users);
 
-            return Ok(jsonString);
+            return Ok(users);
+        }
+
+        [HttpPut]
+        [Route("update-user")]
+        public async Task<IActionResult> UpdateUser(UserModel request)
+        {
+            try
+            {
+                var user = await _dbContext.UserDetails.FindAsync(request.Email);
+
+                if (user == null)
+                    return NotFound(new Response { Status = "Error", Message = "User not found" });
+
+                // Account
+                user.Role = request.Role;
+                user.RemotePercentage = request.RemotePercentage;
+
+                // Details
+                user.FirstName = request.FirstName;
+                user.LastName = request.LastName;
+                user.Gender = request.Gender;
+
+                // Optionals
+                user.BirthDate = request.BirthDate;
+                user.Nationality = request.Nationality;
+
+                // Save data
+                _dbContext.UserDetails.Update(user);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(new Response { Status="Succes", Message="User has been updated"});
+
+            }
+            catch (Exception)
+            {
+                return BadRequest(new Response { Status = "Error", Message = "There was a problem" } );
+            }
+        }
+
+
+        [HttpPut]
+        [Route("update-activate-user")]
+        public async Task<IActionResult> AccountActivate(UserModel request)
+        {
+            try
+            {
+                var user = await _dbContext.UserDetails.FindAsync(request.Email);
+                if(user == null)
+                    return NotFound(new Response { Status = "Error", Message = "User not found" });
+
+                // If account is already activated there should be a problem
+                if (user.AccountStatus == AccountStatus.Active)
+                    return BadRequest(new Response { Status = "Error", Message = "Account is already active" });
+
+                // Update account status
+                user.AccountStatus = AccountStatus.Active;
+
+                // Save data
+                _dbContext.Update(user);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(new Response { Status = "Succes", Message = "User has been activated" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new Response { Status = "Error", Message = "There was a problem" });
+            }
+        }
+
+        [HttpPut]
+        [Route("update-deactivate-user")]
+        public async Task<IActionResult> AccountDeactivate(UserModel request)
+        {
+            try
+            {
+                var user = await _dbContext.UserDetails.FindAsync(request.Email);
+                if (user == null)
+                    return NotFound(new Response { Status = "Error", Message = "User not found" });
+
+                // An administrator cannot deactivate his own account.
+                // WIP
+
+
+                // If account is already activated there should be a problem
+                if (user.AccountStatus == AccountStatus.Inactive)
+                    return BadRequest(new Response { Status = "Error", Message = "Account is already inactive" });
+
+                // Update account status
+                user.AccountStatus = AccountStatus.Inactive;
+
+                // Save data
+                _dbContext.Update(user);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(new Response { Status = "Succes", Message = "User has been deactivated" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new Response { Status = "Error", Message = "There was a problem"});
+            }
         }
 
     }
--- Services/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ATC_BE.Models;
-
-namespace ATC_BE.Services
-{
-    public interface IUserService
-    {
-        public List<UserModel> GetUsers(); 
-    }
-}
